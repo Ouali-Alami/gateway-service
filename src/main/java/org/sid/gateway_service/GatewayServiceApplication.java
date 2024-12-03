657abefb2a03a0d07b42de799222003e615f8a3d
Walle <mohouali.748@gmail.com>
2024-12-03 18:37:47 +0000

    define routes for the gateway-service

--- src/main/java/org/sid/gateway_service/GatewayServiceApplication.java
+++ src/main/java/org/sid/gateway_service/GatewayServiceApplication.java
@@ -2,15 +2,10 @@ package org.sid.gateway_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
 import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
 import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
-import org.springframework.cloud.gateway.route.Route;
-import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.context.annotation.Bean;
-import reactor.core.publisher.Flux;
 
 import java.util.List;
 
